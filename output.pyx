5ca49a6b3d3bafb336ad2bf444fca981034737d4
kiwi-1919 <HIn@zh.email.cn>
2021-07-03 22:47:54 +0800

    test

--- output.pyx
+++ output.pyx
@@ -3,18 +3,18 @@ cimport cython
 import sys,os
 
 cdef void export(int status):
-	if status==0:
-		print("alright")
-	elif status==1:
-		print("error:anno")
-	elif status==2:
-		print("error:ps")
-	elif status==3:
-		print("error:sp")
-	elif status==4:
-		print("error:ss")
-	else:
-		sys.exit()
-	os.system("pause")
+    if status==0:
+        print("alright")
+    elif status==1:
+        print("error:anno")
+    elif status==2:
+        print("error:ps")
+    elif status==3:
+        print("error:sp")
+    elif status==4:
+        print("error:ss")
+    else:
+        sys.exit()
+    os.system("pause")
 def _export():
     export()
