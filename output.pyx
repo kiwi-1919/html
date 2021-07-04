801c22eed680308da5976e7d5ff570310f1c6c6f
kiwi-1919 <HIn@zh.email.cn>
2021-07-04 17:31:21 +0800

    test

--- output.pyx
+++ output.pyx
@@ -10,14 +10,14 @@ cdef void export(int status):
         window.title("error")
         window.geometry("350x200")
         with open("tmp.txt","rt",encoding="utf-8") as file:
-            lbl=tkinter.Label(window,text=file.readlines()[0])
-            lbl.grid(column=0,row=0)
             it=iter(file.readlines())
-            def click():
-                lbl.configure(text=next(it))
-            btn=tkinter.Button(window,text="next",command=click)
-            btn.grid(column=0,row=1)
-            window.mainloop()
+        lbl=tkinter.Label(window,text=it[0])
+        lbl.grid(column=0,row=0)
+        def click():
+            lbl.configure(text=next(it))
+        btn=tkinter.Button(window,text="next",command=click)
+        btn.grid(column=0,row=1)
+        window.mainloop()
     else:
         sys.exit()
 def _export(result=0):
