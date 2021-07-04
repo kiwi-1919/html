38214a9ea20c1390ecedf7b151ffa3c8a3da4db2
kiwi-1919 <HIn@zh.email.cn>
2021-07-04 09:30:28 +0800

    test

--- core.pyx
+++ core.pyx
@@ -4,40 +4,38 @@ from libc.stdio cimport *
 import sys
 sys.setrecursionlimit=10**100
 
-cdef int pp=0
-cdef int anno=1
-cdef int ps=2
-cdef int sp=3
-cdef int ss=4
+cdef const int pp=0
+cdef const int anno=1
+cdef const int ps=2
+cdef const int sp=3
+cdef const int ss=4
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
 cdef int parse():
     cdef FILE* file=fopen("tmp.txt","rb")
     cdef int status=pp
-    cdef char* c
+    cdef char c
     cdef char lb=<char>'<'.encode()
     cdef char rb=<char>'>'.encode()
     cdef char sl=<char>'/'.encode()
     cdef char ex=<char>'!'.encode()
     #end-cdef
     while True:
-        if status==pp and fread(c,1,1,file)==1 and c[0]==lb:
-            if fread(c,1,1,file)==1 and c[0]==ex:
+        if status==pp and fread(&c,1,1,file)==1 and c==lb:
+            if fread(&c,1,1,file)==1 and c==ex:
                 status=anno
             else:
                 status=ss
-        if status==anno and fread(c,1,1,file)==1 and c[0]==rb:
+        if status==anno and fread(&c,1,1,file)==1 and c==rb:
             status=pp
-        if status==ss and fread(c,1,1,file)==1 and c[0]==sl:
-            if fread(c,1,1,file)==1 and c[0]==rb:
+        if status==ss and fread(&c,1,1,file)==1 and c==sl:
+            if fread(&c,1,1,file)==1 and c==rb:
                 status=pp
-        if status==ss and fread(c,1,1,file)==1 and c[0]==rb:
+        if status==ss and fread(&c,1,1,file)==1 and c==rb:
             status==ps
-        if status==ps and fread(c,1,1,file)==1 and c[0]==lb:
-            if fread(c,1,1,file)==1 and c[0]==sl:
+        if status==ps and fread(&c,1,1,file)==1 and c==lb:
+            if fread(&c,1,1,file)==1 and c==sl:
                 status=sp
-        if status==sp and fread(c,1,1,file)==1 and c[0]==rb:
+        if status==sp and fread(&c,1,1,file)==1 and c==rb:
             status=pp
         if ftell(file)==SEEK_END:
             break
