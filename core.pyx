9416e57cf1f3a5c7e40ae654528639f30660ef70
kiwi-1919 <HIn@zh.email.cn>
2021-07-04 11:32:54 +0800

    test

--- core.pyx
+++ core.pyx
@@ -5,14 +5,10 @@ import sys
 sys.setrecursionlimit=10**100
 import tqdm
 
-cdef int pp=0
-cdef int anno=1
-cdef int ps=2
-cdef int sp=3
-cdef int ss=4
 cdef size_t loc=0
 cdef FILE* file=fopen("tmp.txt","rb")
-cdef int status=pp
+cdef int status1=0
+cdef int status2=0
 cdef char c
 cdef bytes b1='<'.encode()
 cdef bytes b2='>'.encode()
@@ -24,34 +20,35 @@ cdef char sl=<char>int.from_bytes(b3,'big')
 cdef char ex=<char>int.from_bytes(b4,'big')
 #end-cdef
 
-cdef int parse():
+cdef void parse():
     global loc
-    global status
+    global status1
+    global status2
     global c
     fseek(file,loc,SEEK_SET)
-    if status==pp and fread(&c,1,1,file)==1 and c==lb:
-        if fread(&c,1,1,file)==1 and c==ex:
-            status=anno
-        else:
-            status=ss
-    if status==anno and fread(&c,1,1,file)==1 and c==rb:
-        status=pp
-    if status==ss and fread(&c,1,1,file)==1:
-        if c==sl:
-            if fread(&c,1,1,file)==1 and c==rb:
-                status=pp
+    if fread(c,1,1,file)==1:
+        if c==lb:
+            if fread(c,1,1,file)==1 and c==sl:
+                status1+=1
+                status2-=1
+            elif c==ex:
+                status1+=1
+            else:
+                status1+=1
+                status2+=1
+        elif c==sl:
+            if fread(c,1,1,file)==1 and c==rb:
+                status1-=1
+                status2-=1
         elif c==rb:
-            status=ps
-    if status==ps and fread(&c,1,1,file)==1 and c==lb:
-        if fread(&c,1,1,file)==1 and c==sl:
-            status=sp
-    if status==sp and fread(&c,1,1,file)==1 and c==rb:
-        status=pp
+            status1-=1
     loc=ftell(file)
-    return status
 def _parse():
     fseek(file,0,SEEK_END)
     length=ftell(file)
     for _ in tqdm.trange(length):
-        result=parse()
-    return result
+        parse()
+    if status1==0 and status2==0:
+        return 0
+    else:
+        return -1
--- output.pyx
+++ output.pyx
@@ -1,20 +1,13 @@
 #cython:language_level=3
 cimport cython
-import sys,os
+import sys
 
 cdef void export(int status):
     if status==0:
         print("alright")
-    elif status==1:
-        print("error:anno")
-    elif status==2:
-        print("error:ps")
-    elif status==3:
-        print("error:sp")
-    elif status==4:
-        print("error:ss")
+    elif status==-1:
+        print("error")
     else:
         sys.exit()
-    os.system("pause")
 def _export(result=0):
     export(result)
