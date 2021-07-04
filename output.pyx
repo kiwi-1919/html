d3d7424a14ed2035dee548f706f4e189eccace3a
kiwi-1919 <HIn@zh.email.cn>
2021-07-04 16:38:30 +0800

    test

--- output.pyx
+++ output.pyx
@@ -1,12 +1,17 @@
 #cython:language_level=3
 cimport cython
-import sys
+import sys,tkinter
 
 cdef void export(int status):
     if status==0:
         print("alright")
     elif status==-1:
-        print("error")
+        window=tkinter.Tk()
+        window.title("error")
+        with open("tmp.txt","rt"):
+            lbl=tkinter.Label(window,text=file.read())
+            lbl.grid(column=0,row=0)
+            window.mainloop()
     else:
         sys.exit()
 def _export(result=0):
